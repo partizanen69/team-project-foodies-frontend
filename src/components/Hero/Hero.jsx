dc4028bd65aa7657170b6630b4680cca159ece81
Iris2030 <ibazhinova@gmail.com>
2024-06-07 16:45:33 +0300

    feat: added and setted images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,18 +2,27 @@ import {React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from 'components/Container/Container';
 
+
 import s from './Hero.module.scss';
 
 const Hero = () => {
     return (
       <Container className={s.hero_container} style={{borderRadius: '20px'}}>
         <div className={s.hero_content}>
-          <h1  className={s.hero_title}>IMPROVE YOUR CULINARY TALENTS</h1>
+          <h1  className={s.hero_title}>IMPROVE YOUR <br className={s.break}/> CULINARY TALENTS</h1>
           <p className={s.hero_text}>Amazing recipes for beginners in the world of cooking, enveloping you in the aromas and tastes of various cuisines.</p>
-          <button className={s.add_recipe_button}>ADD RECIPE</button>
+          <button className={s.hero_button}>ADD RECIPE</button>
           <div className={s.images}>
-            <img src={process.env.PUBLIC_URL + '/hero-food1.png'} alt="Dish 1" className={s.image_main} />
-            <img src={process.env.PUBLIC_URL + '/hero-dessert1.png'} alt="Dish 2" className={s.image_secondary} />
+          {/* <img
+            className={s.image_main}
+            srcSet={`${heroFood1} 1x, ${heroFood2} 2x, ${heroFood2} 3x, ${heroFood2} 4x`}
+            src={heroFood1}
+            alt="Description"
+          /> */}
+           <div className={s.image_main}></div>  
+            <div className={s.image_secondary}></div> 
+            {/* <img src={heroFood1} alt="Dish 1" className={s.image_main} />
+            <img src={heroDessert1} alt="Dish 2" className={s.image_secondary} /> */}
           </div>
         </div>
       </Container>
