d760f79e1f991dbd4f13265ca4a5c6a9be861d10
Iris2030 <ibazhinova@gmail.com>
2024-06-07 16:53:39 +0300

    fix: removed unnecessary ci

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import {React, useEffect } from 'react';
+import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from 'components/Container/Container';
 
@@ -6,27 +6,19 @@ import Container from 'components/Container/Container';
 import s from './Hero.module.scss';
 
 const Hero = () => {
-    return (
-      <Container className={s.hero_container} style={{borderRadius: '20px'}}>
-        <div className={s.hero_content}>
-          <h1  className={s.hero_title}>IMPROVE YOUR <br className={s.break}/> CULINARY TALENTS</h1>
-          <p className={s.hero_text}>Amazing recipes for beginners in the world of cooking, enveloping you in the aromas and tastes of various cuisines.</p>
-          <button className={s.hero_button}>ADD RECIPE</button>
-          <div className={s.images}>
-          {/* <img
-            className={s.image_main}
-            srcSet={`${heroFood1} 1x, ${heroFood2} 2x, ${heroFood2} 3x, ${heroFood2} 4x`}
-            src={heroFood1}
-            alt="Description"
-          /> */}
-           <div className={s.image_main}></div>  
-            <div className={s.image_secondary}></div> 
-            {/* <img src={heroFood1} alt="Dish 1" className={s.image_main} />
-            <img src={heroDessert1} alt="Dish 2" className={s.image_secondary} /> */}
-          </div>
-        </div>
-      </Container>
-    );
-  };
-  
+	return (
+		<Container className={s.hero_container} style={{ borderRadius: '20px' }}>
+			<div className={s.hero_content}>
+				<h1 className={s.hero_title}>IMPROVE YOUR <br className={s.break} /> CULINARY TALENTS</h1>
+				<p className={s.hero_text}>Amazing recipes for beginners in the world of cooking, enveloping you in the aromas and tastes of various cuisines.</p>
+				<button className={s.hero_button}>ADD RECIPE</button>
+				<div className={s.images}>
+					<div className={s.image_main}></div>
+					<div className={s.image_secondary}></div>
+				</div>
+			</div>
+		</Container>
+	);
+};
+
 export default Hero;
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+// ---- usege example -------
 // import Icon from './Icon';
 
 // const App = () => (
