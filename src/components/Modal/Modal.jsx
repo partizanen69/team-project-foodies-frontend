03d149916f2fed91a373bbdfc10d4f57e1b5aad4
Iris2030 <ibazhinova@gmail.com>
2024-06-09 17:42:38 +0300

    feat: added modal forms

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -24,7 +24,12 @@ const Modal = ({ isOpen, onClose, children }) => {
 	return (
 		<div className={s.modal_overlay} onClick={onClose}>
 			<div className={s.modal_content} onClick={(e) => e.stopPropagation()}>
-				<button className={s.modal_close} onClick={onClose}>×</button>
+				<button className={s.modal_close} onClick={onClose}>
+				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M18 6L6 18" stroke="#050505" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+					<path d="M6 6L18 18" stroke="#050505" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+				</svg>
+				</button>
 				{children}
 			</div>
 		</div>
