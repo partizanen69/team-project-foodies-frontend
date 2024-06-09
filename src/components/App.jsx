0f17fcdc4a2dd4d07505b039bd3b368450255a54
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2024-06-09 16:47:23 +0300

    setup axios and env variables

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,32 @@
-import { React, lazy, Suspense } from 'react';
+import { React, lazy, Suspense, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 
 import s from './App.module.scss';
+import axios from 'axios';
+import { getRecipes } from '../api/recipes';
 
 const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
 const Loader = lazy(() => import('./Loader/Loader'));
 const Container = lazy(() => import('./Container/Container'));
 
+axios.defaults.baseURL =
+  process.env.REACT_APP_BACKEND || 'http://localhost:3000/api';
+
+axios.interceptors.request.use(config => {
+  const token = localStorage.getItem('jwt-token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export const App = () => {
+  useEffect(() => {
+    getRecipes().then(recipes => {
+      console.log('recipes', recipes);
+    });
+  }, []);
   return (
     <Container className={s.app_container}>
       <Suspense fallback={<Loader />}>
