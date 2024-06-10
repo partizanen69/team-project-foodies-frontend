e4b5096b60349ba03362fab5bdcce4702cc4cd29
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2024-06-10 22:09:55 +0200

    added global components to profile page

--- src/pages/MainPage/UserPage/UserPage.jsx
+++ src/pages/MainPage/UserPage/UserPage.jsx
@@ -3,11 +3,19 @@ import Container from 'components/Container/Container';
 import s from './UserPage.module.scss';
 import UserInfo from 'components/UserInfo/UserInfo';
 import { Outlet } from 'react-router-dom';
+import MainTitle from 'components/MainTitle/MainTitle';
+import { PathInfo } from 'components/PathInfo/PathInfo';
+import Subtitle from 'components/Subtitle/Subtitle';
 
 const UserPage = () => {
   return (
     <Container className={s.main_container}>
-      User Page
+      <PathInfo currentPageName="profile" />
+      <MainTitle>Profile</MainTitle>
+      <Subtitle>
+        Reveal your culinary art, share your favorite recipe and create
+        gastronomic masterpieces with us.
+      </Subtitle>
       <UserInfo />
       <Outlet />
     </Container>
