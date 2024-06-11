4691beaa45ade45bf730d195e38b10f614802891
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2024-06-11 22:48:42 +0200

    added logout button

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -1,14 +1,22 @@
-import PropTypes from 'prop-types';
 import s from './ProfileInfo.module.scss';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import { getUserDetails } from 'api/users';
 import Icon from 'components/Icon';
+import Modal from 'components/Modal/Modal';
+import LogoutForm from 'components/LogoutForm/LogoutForm';
+import Loader from 'components/Loader/Loader';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../redux/actions/authActions';
 
 const ProfileInfo = ({ userId }) => {
+  const dispatch = useDispatch();
+
   const [isLoading, setIsLoading] = useState(true);
   const [errorMsg, setErrorMsg] = useState(null);
   const [userDetails, setuserDetails] = useState({});
+
+  const [isModalLogOutOpen, setIsModalLogOutOpen] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -28,66 +36,96 @@ const ProfileInfo = ({ userId }) => {
     })();
   }, [userId]);
 
+  const openModal = () => {
+    setIsModalLogOutOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalLogOutOpen(false);
+  };
+
+  const handleLogOut = () => {
+    dispatch(logout());
+    closeModal();
+  };
+
   return (
-    <div className={s.profile_info}>
-      <div className={s.profile_info_card}>
-        <div className={s.profile_avatar_block}>
-          <img
-            alt="User avatar"
-            src={userDetails.avatarURL}
-            className={s.profile_avatar}
-          />
-          <button type="button" className={s.btn_add_avatar}>
-            <Icon name="icon-plus" width="16" height="16" className={s.plus} />
-          </button>
+    <Suspense fallback={<Loader />}>
+      <div className={s.profile_info}>
+        <div className={s.profile_info_card}>
+          <div className={s.profile_avatar_block}>
+            <img
+              alt="User avatar"
+              src={userDetails.avatarURL}
+              className={s.profile_avatar}
+            />
+            <button type="button" className={s.btn_add_avatar}>
+              <Icon
+                name="icon-plus"
+                width="16"
+                height="16"
+                className={s.plus}
+              />
+            </button>
+          </div>
+
+          <p className={s.user_name}>
+            {isLoading ? `Loading...` : userDetails.name}
+          </p>
+
+          <ul className={s.details_list}>
+            <li className={s.details_list_item}>
+              <p className={s.item_key}>Email: </p>
+              <span className={s.item_value}>
+                {isLoading ? `Loading...` : userDetails.email}
+              </span>
+            </li>
+
+            <li className={s.details_list_item}>
+              <p className={s.item_key}>Added recipes: </p>
+              <span className={s.item_value}>
+                {isLoading ? `Loading...` : userDetails.recipesCount}
+              </span>
+            </li>
+
+            {userDetails.hasOwnProperty('favorites') && (
+              <li className={s.details_list_item}>
+                <p className={s.item_key}>Favorites: </p>
+                <span className={s.item_value}>
+                  {isLoading ? `Loading...` : userDetails.favorites}
+                </span>
+              </li>
+            )}
+
+            <li className={s.details_list_item}>
+              <p className={s.item_key}>Followers: </p>
+              <span className={s.item_value}>
+                {isLoading ? `Loading...` : userDetails.followersCount}
+              </span>
+            </li>
+
+            {userDetails.hasOwnProperty('followingCount') && (
+              <li className={s.details_list_item}>
+                <p className={s.item_key}>Following: </p>
+                <span className={s.item_value}>
+                  {isLoading ? `Loading...` : userDetails.followingCount}
+                </span>
+              </li>
+            )}
+          </ul>
         </div>
 
-        <p className={s.user_name}>
-          {isLoading ? `Loading...` : userDetails.name}
-        </p>
-
-        <ul className={s.details_list}>
-          <li className={s.details_list_item}>
-            <p className={s.item_key}>Email: </p>
-            <span className={s.item_value}>
-              {isLoading ? `Loading...` : userDetails.email}
-            </span>
-          </li>
-
-          <li className={s.details_list_item}>
-            <p className={s.item_key}>Added recipes: </p>
-            <span className={s.item_value}>
-              {isLoading ? `Loading...` : userDetails.recipesCount}
-            </span>
-          </li>
-
-          {userDetails.hasOwnProperty('favorites') && (
-            <li className={s.details_list_item}>
-              <p className={s.item_key}>Favorites: </p>
-              <span className={s.item_value}>
-                {isLoading ? `Loading...` : userDetails.favorites}
-              </span>
-            </li>
-          )}
-
-          <li className={s.details_list_item}>
-            <p className={s.item_key}>Followers: </p>
-            <span className={s.item_value}>
-              {isLoading ? `Loading...` : userDetails.followersCount}
-            </span>
-          </li>
-
-          {userDetails.hasOwnProperty('followingCount') && (
-            <li className={s.details_list_item}>
-              <p className={s.item_key}>Following: </p>
-              <span className={s.item_value}>
-                {isLoading ? `Loading...` : userDetails.followingCount}
-              </span>
-            </li>
-          )}
-        </ul>
+        <button type="submit" className={s.btn_logout} onClick={openModal}>
+          Log Out
+        </button>
       </div>
-    </div>
+
+      {isModalLogOutOpen && (
+        <Modal isOpen={isModalLogOutOpen} onClose={closeModal}>
+          <LogoutForm handleLogOut={handleLogOut} closeModal={closeModal} />
+        </Modal>
+      )}
+    </Suspense>
   );
 };
 
