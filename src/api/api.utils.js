dca9a691b4471b069e041043a85d8dd65836107a
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2024-06-12 12:32:22 +0300

    fix axios error handler

--- src/api/api.utils.js
+++ src/api/api.utils.js
@@ -1,8 +1,11 @@
 export const handleAxiosError = err => {
   const msgPrefix = 'Request failed with error:';
+  console.error('http request failed with error', err);
   if (err.name === 'AxiosError') {
     throw new Error(
-      `${msgPrefix} ${err.response.data?.message} and status text ${err.response.statusText}`
+      `${msgPrefix} ${
+        err.response?.data?.message || err.message
+      } and status text ${err.response?.statusText || err.code}`
     );
   }
   throw new Error(`${msgPrefix} ${err.message}`);
