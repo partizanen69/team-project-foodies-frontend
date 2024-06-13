7d3818f9805b8275c26a59677366be2bfcb70190
KashnykovAnton <kashnikov_anton@yahoo.com>
2024-06-13 04:10:42 +0200

    popular recipes logic and some styles added

--- src/api/users.js
+++ src/api/users.js
@@ -3,7 +3,7 @@ import { handleAxiosError } from './api.utils';
 
 export const getUserDetailsById = async id => {
   try {
-    const result = await axios.get(`/user-details/${id}`);
+    const result = await axios.get(`users/user-details/${id}`);
     return result.data;
   } catch (err) {
     handleAxiosError(err);
