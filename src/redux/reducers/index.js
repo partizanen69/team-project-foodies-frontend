45ac1e7feef25e7d3b37f230c69380c5980f9b83
ostafiichuko <issimo333@gmail.com>
2024-06-14 11:59:28 +0300

    fix: areas reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from './authReducer';
 import uiReducer from './uiReducer';
 import ingredientsReducer from './ingredientsReducer'
+import areasReducer from './areasReducer'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     ui: uiReducer,
     ingredients: ingredientsReducer,
+    areas: areasReducer,
 });
 
 export default rootReducer;
