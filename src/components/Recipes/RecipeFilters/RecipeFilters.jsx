93bf3c5730cf02745ae735b7c7ae43c269e450d4
ostafiichuko <issimo333@gmail.com>
2024-06-14 21:45:01 +0300

    fix: remover props

--- src/components/Recipes/RecipeFilters/RecipeFilters.jsx
+++ src/components/Recipes/RecipeFilters/RecipeFilters.jsx
@@ -1,7 +1,6 @@
 // import tools
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
 
 // import styles
 import s from './RecipeFilters.module.scss';
