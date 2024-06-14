83b0bc4620c26132780c5065e0fe38e5f612f101
Maryna-Korbet <m22v86k@gmail.com>
2024-06-14 03:03:49 -0400

    add shemaYup

---
+++ src/components/AddRecipeForm/schemaYup.js
@@ -0,0 +1,17 @@
+import * as yup from 'yup';
+
+const schemaYup = yup.object().shape({
+    title: yup.string().required('Title is required'),
+    description: yup.string().max(200, 'Description must be at most 200 characters').required('Description is required'),
+    category: yup.string().required('Category is required'),
+    time: yup.number().min(1, 'Time must be at least 1 minute').required('Time is required'),
+    ingredients: yup.array().of(
+        yup.object().shape({
+        ingredient: yup.string().required('Ingredient is required'),
+        measure: yup.string().required('Measure is required'),
+        })
+    ).min(1, 'At least one ingredient is required'),
+    instructions: yup.string().max(200, 'Instructions must be at most 200 characters').required('Instructions are required'),
+});
+
+export default schemaYup;
