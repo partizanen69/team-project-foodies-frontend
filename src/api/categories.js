0a46436c0d76fc8b5f0b54fee448389e430c8fa6
maryna-korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2024-06-14 14:30:25 -0400

    Update categories.js

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,16 +1,5 @@
 import axios from 'axios';
 
-import { handleAxiosError } from './api.utils';
-
-export const getCategoriesList = async () => {
-    try {
-        const result = await axios.get('/categories');
-        return result.data;
-    } catch (err) {
-        handleAxiosError(err);
-    }
-
-
 export const getCategories = async ({
   page = 1,
   limit = 11,
