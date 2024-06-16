f83f5b98e7e84d3a225ae2d7af7d58cf34a03916
Maryna-Korbet <m22v86k@gmail.com>
2024-06-16 10:12:44 -0400

    update TimeInput styles

--- src/components/AddRecipeForm/TimeInput/TimeInput.jsx
+++ src/components/AddRecipeForm/TimeInput/TimeInput.jsx
@@ -34,9 +34,11 @@ const TimeInput = ({ register, getValues, setValue, errors }) => {
           type="button"
           onClick={decrementTime}
           className={s.add_recipe_form_time_button}
-        ><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5 12H19" stroke="#050505" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg></button>
+        >
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 12H19" stroke="#050505" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </button>
         <input
           type="text"
           value={displayTime}
@@ -47,13 +49,14 @@ const TimeInput = ({ register, getValues, setValue, errors }) => {
           type="button"
           onClick={incrementTime}
           className={s.add_recipe_form_time_button}
-        ><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12 5V19" stroke="#050505" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M5 12H19" stroke="#050505" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-</button>
+        >
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 5V19" stroke="#050505" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5 12H19" stroke="#050505" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </button>
       </div>
-      {errors && <p className={s.add_recipe_form_error}>{errors.message}</p>}
+      {errors && <p className={s.add_recipe_form_error}>{errors.time?.message}</p>}
     </div>
   );
 };
