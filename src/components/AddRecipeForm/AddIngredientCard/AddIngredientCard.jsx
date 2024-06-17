40f09e5c43e65379ac9ac4709f5f75e7f496caf1
KashnykovAnton <kashnykov.anton@gmail.com>
2024-06-17 04:24:36 +0200

    sprite done

--- src/components/AddRecipeForm/AddIngredientCard/AddIngredientCard.jsx
+++ src/components/AddRecipeForm/AddIngredientCard/AddIngredientCard.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
-import { ReactComponent as CloseIcon } from '../../../assets/icons/close-icon.svg';
+import icons from '../../../assets/icons/icons.svg';
 
 import s from './AddIngredientCard.module.scss';
 
 const AddIngredientCard = ({ removeIngredientCard, ingredientCards }) => {
-    return (
-        <div className={s.add_recipe_form_ingredient_card_container}>
-            {ingredientCards.map((card, index) => (
-                <div key={`ingredient-card-${index}`} className={s.ingredient_card}>
-                    <div className={s.ingredient_info}>
-                        <img src={card.img} className={s.add_recipe_form_ingredient_card_img} alt={card.name} />
-                        <div className={s.add_recipe_form_ingredient_card_text}>
-                            <h4 className={s.ingredient_card_name}>{card.name}</h4>
-                            <p className={s.ingredient_card_measure}>{card.measure}</p>
-                        </div>
-                    </div>
-                    <button
-                        type="button"
-                        onClick={() => removeIngredientCard(index)}
-                        className={s.add_recipe_form_close_button}
-                    >
-                        <CloseIcon className={s.add_recipe_form_close_icon} />
-                    </button>
-                </div>
-            ))}
+  return (
+    <div className={s.add_recipe_form_ingredient_card_container}>
+      {ingredientCards.map((card, index) => (
+        <div key={`ingredient-card-${index}`} className={s.ingredient_card}>
+          <div className={s.ingredient_info}>
+            <img
+              src={card.img}
+              className={s.add_recipe_form_ingredient_card_img}
+              alt={card.name}
+            />
+            <div className={s.add_recipe_form_ingredient_card_text}>
+              <h4 className={s.ingredient_card_name}>{card.name}</h4>
+              <p className={s.ingredient_card_measure}>{card.measure}</p>
+            </div>
+          </div>
+          <button
+            type="button"
+            onClick={() => removeIngredientCard(index)}
+            className={s.add_recipe_form_close_button}
+          >
+            <svg className={s.add_recipe_form_close_icon}>
+              <use xlinkHref={`${icons}#${'icon-close'}`}></use>
+            </svg>
+          </button>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default AddIngredientCard;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 import { React, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
+import icons from '../../assets/icons/icons.svg';
 import s from './Modal.module.scss';
 
 const Modal = ({ isOpen, onClose, children }) => {
@@ -26,27 +26,8 @@ const Modal = ({ isOpen, onClose, children }) => {
     <div className={s.modal_overlay} onClick={onClose}>
       <div className={s.modal_content} onClick={e => e.stopPropagation()}>
         <button className={s.modal_close} onClick={onClose}>
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M18 6L6 18"
-              stroke="#050505"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M6 6L18 18"
-              stroke="#050505"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
+          <svg className={s.modal_close_icon}>
+            <use xlinkHref={`${icons}#${'icon-close'}`}></use>
           </svg>
         </button>
         {children}
