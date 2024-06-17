51ac53ed7d4256e7f325500138907ff6ac60e32d
Maryna-Korbet <m22v86k@gmail.com>
2024-06-17 12:18:35 -0400

    added sort selectors options

--- src/api/areas.js
+++ src/api/areas.js
@@ -4,7 +4,7 @@ import { handleAxiosError } from './api.utils';
 export const getAreasList = async () => {
   try {
     const result = await axios.get('/areas');
-    return result.data;
+    return result.data.slice().sort((a, b) => a.name.localeCompare(b.name));
   } catch (err) {
     handleAxiosError(err);
   }
